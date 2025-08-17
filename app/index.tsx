dbe3ba8b0fc1dfe2e56dfcf33e35b7f8bafa0458
teovlt <teo.villet2@gmail.com>
2025-08-17 14:53:45 +0200

    Add colors

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold text-accent">Welcome to Nativewind!</Text>
     </View>
   );
 }
