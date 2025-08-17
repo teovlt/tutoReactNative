eea7300279182c952bac5e3c8689ad84ad2a8b42
teovlt <teo.villet2@gmail.com>
2025-08-17 20:52:18 +0200

    Add basic tabs with header not shown

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,21 @@ import { Stack } from "expo-router";
 import "../styles/index.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="movies/[id]"
+        options={{
+          headerShown: false,
+          title: "Movie Details",
+        }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="items-center justify-center flex-1">
       <Text className="text-xl font-bold text-accent">Welcome to Nativewind!</Text>
     </View>
   );
